31ab8152707d9ff43e7857b30597753248e11ff0
Aaron Thapa <notsmart98@gmail.com>
2024-02-28 20:51:44 +0545

    fix steam

--- configuration.nix
+++ configuration.nix
@@ -97,6 +97,12 @@
     stremio
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
