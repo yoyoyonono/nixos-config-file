54724514549c1c4f1fd96e1813da4e5d31202fa9
Aaron Thapa <notsmart98@gmail.com>
2024-02-29 12:45:43 +0545

    finally fixed

--- home.nix
+++ home.nix
@@ -87,6 +87,8 @@
       ];
     };
     initExtra = ''
+      # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
       eval "$(zoxide init zsh)"
     '';
     initExtraBeforeCompInit = ''
