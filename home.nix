bae9443f9b37ab6255498b05bad35d404ce20ec6
Aaron Thapa <notsmart98@gmail.com>
2024-05-02 21:30:37 +0545

    fix indent

--- home.nix
+++ home.nix
@@ -15,7 +15,7 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
   
-   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
