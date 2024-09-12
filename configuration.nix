beb3806f4ae5f7bf430ccee8cbfe3506a13ea208
Aaron Thapa <notsmart98@gmail.com>
2024-09-12 09:57:21 -0400

    fix deprecation warning

--- configuration.nix
+++ configuration.nix
@@ -62,7 +62,8 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
       fcitx5-gtk 
