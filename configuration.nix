2bcaea6843f7313cfb2cdddf7aa07d7c53503ede
Aaron Thapa <notsmart98@gmail.com>
2026-07-04 22:58:54 -0400

    add u2f prompt

--- configuration.nix
+++ configuration.nix
@@ -64,11 +64,14 @@
 
   };
 
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
-    swaylock.u2fAuth = true;
-    greetd.u2fAuth = true;
+  security.pam = {
+    u2f.settings.cue = true;
+    services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+      swaylock.u2fAuth = true;
+      greetd.u2fAuth = true;
+    };
   };
 
   # Set your time zone.
